1ba3979536ef85f9fcb611f85ec8a2a13aa2f4ea
Donovan Diamond <d@cloaf.africa>
2024-07-05 07:43:12 +0000

    cleanup of go.lua

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,26 +1,16 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
---if true then return {} end
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-	{
-	  "ray-x/go.nvim",
-	  dependencies = {  -- optional packages
-	    "ray-x/guihua.lua",
-	    "neovim/nvim-lspconfig",
-	    "nvim-treesitter/nvim-treesitter",
-	  },
-	  config = function()
-	    require("go").setup()
-	  end,
-	  event = {"CmdlineEnter"},
-	  ft = {"go", 'gomod'},
-	  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-	}
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
