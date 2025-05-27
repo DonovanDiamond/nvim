b112b5c2f05a696c1841b3d729ca066c19e021b5
Donovan Diamond <56517875+DonovanDiamond@users.noreply.github.com>
2025-05-27 20:20:13 +0200

    first commit

---
+++ lua/plugins/donovan.lua
@@ -0,0 +1,18 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  }
+}
